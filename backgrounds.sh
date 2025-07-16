7c048455046607464a49ea4865bb0a9c34f414f9
David Heinemeier Hansson <david@hey.com>
2025-07-15 21:47:29 -0700

    First

---
+++ backgrounds.sh
@@ -0,0 +1,2 @@
+mkdir -p "$BACKGROUNDS_DIR/synthwave84"
+download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/synthwave84/background.png" "synthwave84/1-synthwave84.png"
